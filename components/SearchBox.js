133011c9153ab45785bc57de124c0155e92e3474
Chris <chris.ware.dev@gmail.com>
2021-09-11 23:08:23 -0700

    SearchBox Functionality/Mapping through Cities

---
+++ components/SearchBox.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import cities from "../lib/city.list.json";
+
+function SearchBox() {
+  const [query, setQuery] = useState("");
+  const [result, setResults] = useState([]);
+
+  const onChange = (e) => {
+    const { value } = e.target;
+    setQuery(value);
+
+    let matchingCities = [];
+
+    if (value.length > 3) {
+      for (let city of cities) {
+        if (matchingCities.length >= 5) {
+          break;
+        }
+
+        const match = city.name.toLowerCase().startsWith(value.toLowerCase());
+
+        if (match) {
+          matchingCities.push(city);
+        }
+      }
+    }
+    console.log(matchingCities);
+  };
+
+  return (
+    <div>
+      <div className='search'>
+        <input type='text' value={query} onChange={onChange} />
+      </div>
+    </div>
+  );
+}
+
+export default SearchBox;
